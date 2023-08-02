43a7de40b06535e84c5704f9c68008f0415711f0
Hossam-Elsheikh <hossam.m.elsheikh@gmail.com>
2023-08-02 16:06:54 +0300

    course page

--- course-script.js
+++ course-script.js
@@ -7,6 +7,16 @@ var courseContainer = document.getElementById('course')
 var currentCourse = courses.courses.find((course)=>course.id == courseID)
 
 courseContainer.innerHTML = `
+<div class="name">
 <h1>${currentCourse.name}</h1>
-<img src="${currentCourse.image}" alt="${currentCourse.name}">
-<p>${currentCourse.longDescription}</p>`
+<button class="${currentCourse.userEnrolled ? "enrolled" : " enroll"}">${currentCourse.userEnrolled ? "Enrolled" : " Enroll"}</button>
+</diV>
+<p> Created By: ${currentCourse.instructor}</p>
+<img class="img" src="${currentCourse.image}" alt="${currentCourse.name}">
+<h2>Requirements</h2>
+<ul>
+${currentCourse.Requirements.map((req)=> `<li><p>${req}</p></li>`)}
+</ul>
+<h2>Description</h2>
+<p>${currentCourse.description}</p>
+`
