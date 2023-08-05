1b36050b4f9440bcae9707c7c6719a6ef6315d23
Hossam-Elsheikh <hossam.m.elsheikh@gmail.com>
2023-08-05 21:52:44 +0300

    ajax

--- script.js
+++ script.js
@@ -1,30 +1,36 @@
-import courses from "./coursesData.json" assert { type: "json" };
-
 var coursesContainer = document.getElementById("courses");
 
-courses.courses.map((course) => {
-    let card = document.createElement("div");
-    card.classList.add("card");
-    card.onclick = function () {
-        window.location.href = `./course/course.html?id=${course.id}`;
-    };
-    card.id = course.id;
-    card.innerHTML = `
-    <div class="img">
-        <img src="${course.image}" alt="">
-
-</div>
-<div class="text">
-    <h4>${course.name}</h4>
-    <div class="details">
-        <p>Created by ${course.instructor}</p>
-        <p>${course.rating}/5 <i class="fa-solid fa-star"></i></p>
-        
+const xhttp = new XMLHttpRequest();
+xhttp.open("GET", "./coursesData.json", true);
+xhttp.onload = function() {
+    var response = JSON.parse(this.response)
+    response.courses.map((course) => {
+        let card = document.createElement("div");
+        card.classList.add("card");
+        card.onclick = function () {
+            window.location.href = `./course/course.html?id=${course.id}`;
+        };
+        card.id = course.id;
+        card.innerHTML = `
+        <div class="img">
+            <img src="${course.image}" alt="">
+    
     </div>
-</div>`;
+    <div class="text">
+        <h4>${course.name}</h4>
+        <div class="details">
+            <p>Created by ${course.instructor}</p>
+            <p>${course.rating}/5 <i class="fa-solid fa-star"></i></p>
+            
+        </div>
+    </div>`;
+    
+        coursesContainer.appendChild(card);
+    });
+  }
+xhttp.send();
 
-    coursesContainer.appendChild(card);
-});
+
 
 
 var logo = document.getElementById('logo')
@@ -40,3 +46,5 @@ user_icon.onclick = function disp(){
     dropDown.style.display = ((dropDown.style.display!='flex') ? 'flex' : 'none')
 
 }
+
+
