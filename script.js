c3a3579d715629b83c53915fcd575f53bcfdfe26
Hossam-Elsheikh <hossam.m.elsheikh@gmail.com>
2023-08-06 13:48:31 +0300

    activating filter

--- script.js
+++ script.js
@@ -1,17 +1,21 @@
-var coursesContainer = document.getElementById("courses");
+const coursesContainer = document.getElementById("courses");
+const categories = []; // holds all the categories
+const categoriesOptions = document.getElementById("categories");
+const categoriesFilter = [];
 
-const xhttp = new XMLHttpRequest();
-xhttp.open("GET", "./coursesData.json", true);
-xhttp.onload = function() {
-    var response = JSON.parse(this.response)
-    response.courses.map((course) => {
-        let card = document.createElement("div");
-        card.classList.add("card");
-        card.onclick = function () {
-            window.location.href = `./course/course.html?id=${course.id}`;
-        };
-        card.id = course.id;
-        card.innerHTML = `
+// this function below to minimize duplicatation of creating cards
+
+function createCards(course) {
+  let card = document.createElement("div");
+  let span = document.createElement("span");
+  span.innerHTML = course.categories;
+  span.classList.add("category");
+  card.classList.add("card");
+  card.onclick = function () {
+    window.location.href = `./course/course.html?id=${course.id}`;
+  };
+  card.id = course.id;
+  card.innerHTML = `
         <div class="img">
             <img src="${course.image}" alt="">
     
@@ -24,27 +28,69 @@ xhttp.onload = function() {
             
         </div>
     </div>`;
-    
-        coursesContainer.appendChild(card);
-    });
-  }
+
+  coursesContainer.appendChild(card);
+  card.appendChild(span);
+}
+
+// ajax request starts
+const xhttp = new XMLHttpRequest();
+xhttp.open("GET", "./coursesData.json", true);
+// here we must put all the logic that depending on the data in the onload function below
+xhttp.onload = function () {
+  var response = JSON.parse(this.response);
+
+  response.courses.map((course) => {
+    categoriesFilter.push(course);
+    if (!categories.includes(course.categories)) {
+      // push all the categories into the array if not already there
+      categories.push(course.categories);
+    }
+    createCards(course);
+  });
+
+  // Create options from categories
+
+  categories.map((category) => {
+    let option = document.createElement("option");
+    option.value = category;
+    option.innerHTML = category;
+    categoriesOptions.appendChild(option);
+  });
+};
 xhttp.send();
 
+// ajax ended successfully
+
+// activating the filter  --
+categoriesOptions.onchange = (e) => {
+  coursesContainer.innerHTML = "";
+  let avtiveCategory = e.target.value;
+  if (avtiveCategory === "All Courses") {
+    categoriesFilter.map((course) => {
+      createCards(course);
+    });
+  }
+  const filteredCategory = categoriesFilter.filter(
+    (category) => category.categories === avtiveCategory
+  );
+  filteredCategory.map((course) => {
+    createCards(course);
+  });
+};
+
+//  filter ended
 
 
 
-var logo = document.getElementById('logo')
+var logo = document.getElementById("logo");
 
 logo.onclick = function () {
-    window.location.href = './'
-}
+  window.location.href = "./";
+};
 
-var dropDown = document.getElementById('user')
-var user_icon = document.getElementById('user_icon')
-user_icon.onclick = function disp(){
-  
-    dropDown.style.display = ((dropDown.style.display!='flex') ? 'flex' : 'none')
-
-}
-
-
+var dropDown = document.getElementById("user");
+var user_icon = document.getElementById("user_icon");
+user_icon.onclick = function disp() {
+  dropDown.style.display = dropDown.style.display != "flex" ? "flex" : "none";
+};
