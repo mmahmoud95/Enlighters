690cf4a02e30f5c4e5ea3e2cfc0f2f7a98863243
Ayashehata226 <Ayashegata@gmail.com>
2023-08-06 16:38:28 +0300

    update vlaidtion

--- singup/index.js
+++ singup/index.js
@@ -1,6 +1,6 @@
 var submit = document.getElementById("submit");
 
-submit.addEventListener('click', function () {
+submit.addEventListener('mouseover', function () {
     var Name = document.getElementById("name");
     var userNmae = document.getElementById("user").value
     var Email = document.getElementById("email").value;
@@ -20,17 +20,24 @@ submit.addEventListener('click', function () {
     rePassword.value = rePasswordValue;
 
     console.log(Name);
-    var patternName = /^[A-Za-z]{3,}( )[A-Za-z]{4,}$/
+    var patternName = /^[A-Za-z]{3,}()[A-Za-z]{4,}$/
     var test = patternName.test(Name);
     if (test !== true) {
         document.getElementById("name").style.border = "1px solid red";
 
     }
+    else {
+        document.getElementById("name").style.border = "1px solid green";
 
-    var patternUser = /^[A-Za-z]{3,}[0-9]{4,}$/
+    }
+
+    var patternUser = /^[A-Z]{3,}[0-9]{4,}$/i
     var test = patternUser.test(userNmae);
     if (test !== true) {
         document.getElementById("user").style.border = "1px solid red";
+    }
+    else {
+        document.getElementById("user").style.border = "1px solid green";
     }
 
     var patternEmail = /^[A-Za-z]{3,}@[a-z]{5,}(.com)$/
@@ -38,12 +45,23 @@ submit.addEventListener('click', function () {
     if (test !== true) {
         document.getElementById("email").style.border = "1px solid red";
     }
+    else {
+        document.getElementById("email").style.border = "1px solid green";
+    }
     var patternPass = /[A-Za-z0-9].*[#$@%*&]{8,}$/;
-    var test = patternPass.test(Password);
+    var test = patternPass.test(passwordValue);
     if (test !== true) {
         document.getElementById("password").style.border = "1px solid red";
     }
-    if (rePassword !== Password) {
-        document.getElementById("password").style.border = "1px solid red";
+    else {
+        document.getElementById("password").style.border = "1px solid green";
+    }
+
+    if (rePasswordValue === "") {
+        document.getElementById("rePassword").style.border = "1px solid red";
+    } else if (rePasswordValue !== passwordValue) {
+        document.getElementById("rePassword").style.border = "1px solid red";
+    } else {
+        document.getElementById("rePassword").style.border = "1px solid green";
     }
 });
