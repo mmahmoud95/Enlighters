330d43f1a8c194f703b1ee473cf46918ba8c500b
Hossam-Elsheikh <hossam.m.elsheikh@gmail.com>
2023-08-07 17:44:29 +0300

    e

--- singup/index.js
+++ singup/index.js
@@ -1,44 +1,61 @@
 var submit = document.getElementById("submit");
-var data_user = {};
-var all_users = [];
+
+var DummyData = {
+    users:[
+
+    ],
+    current_user:0
+}
+
+// hold the local storage 
+var data = JSON.parse(localStorage.getItem("data")) || DummyData;
+
+console.log(data)
+
+function onSubmit(e){
+    e.preventDefault();
+}
 submit.addEventListener('click', function () {
+
+    var current_user = {
+    }
+
     var Name = document.getElementById("name").value;
     var userName = document.getElementById("user").value
     var Email = document.getElementById("email").value;
     var Password = document.getElementById("password").value;
     var rePassword = document.getElementById("rePassword").value;
 
-    // var nameValue = Name.value
-    // var userValue = userName.value
-    // var emailValue = Email.value
-    // var passwordValue = Password.value
-    // var rePasswordValue = rePassword.value
+    // adding user data to current user
+    // current_user.id = Math.round(Math.random()*300)
+    // current_user.fullName = Name;
+    // current_user.userName = userName;
+    // current_user.email = Email;
+    // current_user.password = Password;
 
-    // Name.value = nameValue;
-    // userName.value = userValue;
-    // Email.value = emailValue;
-    // Password.value = passwordValue;
-    // rePassword.value = rePasswordValue;
+
 
     var patternName = /^[A-Za-z]{3,}(\s[A-Za-z]{3,})+$/i;
     var test = patternName.test(Name);
     if (test !== true) {
         document.getElementById("name").style.border = "1px solid red";
     }
+
     else {
+
         document.getElementById("name").style.border = "1px solid green";
-        data_user.id = parseInt(Math.random() * 1000);
-        data_user.fullName = Name;
+        current_user.id = parseInt(Math.random() * 1000);
+        current_user.fullName = Name;
     }
 
-    var patternUser = /[A-Za-z._@#$%&*0-9]*/i
+    var patternUser = /[A-Za-z._@#$%&*0-9]{3,}/i
     var test = patternUser.test(userName);
     if (test !== true) {
         document.getElementById("user").style.border = "1px solid red";
     }
     else {
         document.getElementById("user").style.border = "1px solid green";
-        data_user.userName = userName;
+        current_user.userName = userName;
     }
 
     var patternEmail = /^[A-Za-z0-9_.]{3,}@[A-Za-z]+(\.com)$/;
@@ -48,16 +65,18 @@ submit.addEventListener('click', function () {
     }
     else {
         document.getElementById("email").style.border = "1px solid green";
-        data_user.email = Email;
+        current_user.email = Email;
     }
+
     var patternPass = /[A-Za-z0-9._@#$%&*]{8,}$/;
     var test = patternPass.test(Password);
     if (test !== true) {
         document.getElementById("password").style.border = "1px solid red";
     }
+
     else {
         document.getElementById("password").style.border = "1px solid green";
-        data_user.password = Password;
+        current_user.password = Password;
         //alert(Password);
     }
 
@@ -69,13 +88,13 @@ submit.addEventListener('click', function () {
         document.getElementById("rePassword").style.border = "1px solid green";
     }
 
-    var objectLength = Object.keys(data_user).length;
+    var objectLength = Object.keys(current_user).length;
     console.log(objectLength);
     if (objectLength === 5) {
-        all_users.push(data_user);
-        console.log(all_users);
-        console.log(data_user);
-        var dataJSON = JSON.stringify(all_users);
-        localStorage.setItem('user_data', dataJSON);
+        data.users.push(current_user);
+        console.log(data.users);
+        // console.log(current_user);
+        // var dataJSON = JSON.stringify(data);
+        localStorage.setItem('data', JSON.stringify(data)); //override
     }
 });
