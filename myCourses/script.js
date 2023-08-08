d55b35f5de8613c1817598cf309d01c5b0dd0084
mustafamhmod95@gmail.com <mustafamhmod95@gmail.com>
2023-08-09 00:03:53 +0300

    Handled myCourses with for each user  v2

--- myCourses/script.js
+++ myCourses/script.js
@@ -2,32 +2,30 @@ import courses from "../coursesData.json" assert { type: "json" };
 
 const container = document.getElementById("container");
 const pragraph = document.createElement("p");
-// const courseCard = document.createElement("div");
-var courseCard;
 
+var courseCard;
+var userCourses;
 var data = JSON.parse(localStorage.getItem("data"));
 var users = data.users;
-// var userCourse = storage.users[0].courses;
 
-if (data) {
-    for (const i in users) {
-        if (users[i].id == data.current_user) {
-            var userCourses = data.users[i].courses;
-            for (const courseId in userCourses) {
-                var currentCourse = courses.courses.find(
-                    (course) => course.id === userCourses[courseId]
-                );
-                courseCard = document.createElement("div");
-                courseCard.innerHTML = `<img src="${currentCourse.image}"/>
+for (var i in users) {
+    userCourses = data.users[i].courses;
+    if (users[i].id == data.current_user && userCourses.length !== 0) {
+        for (var courseId in userCourses) {
+            var currentCourse = courses.courses.find(
+                (course) => course.id === userCourses[courseId]
+            );
+
+            courseCard = document.createElement("div");
+            courseCard.innerHTML = `<img src="${currentCourse.image}"/>
                                     <h2 class="course-name">${currentCourse.name}</h2>
                                     <p class="instructor">${currentCourse.instructor}</p>
                                     <p class="rating">Rating: ${currentCourse.rating}</p>`;
-                container.appendChild(courseCard);
-            }
-        } else {
-            pragraph.innerText = "no courses added";
-            pragraph.className = "no-courses";
-            container.appendChild(pragraph);
+            container.appendChild(courseCard);
         }
+    } else if (users[i].id == data.current_user && userCourses.length == 0) {
+        pragraph.innerText = "no courses added";
+        pragraph.className = "no-courses";
+        container.appendChild(pragraph);
     }
 }
