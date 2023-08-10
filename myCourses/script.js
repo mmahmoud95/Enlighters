5ec05c8c344cf580dd577b6f3846735a06c7af7d
Hossam-Elsheikh <hossam.m.elsheikh@gmail.com>
2023-08-10 19:46:26 +0300

    fix

--- myCourses/script.js
+++ myCourses/script.js
@@ -7,37 +7,33 @@ var courseCard;
 var userCourses;
 var data = JSON.parse(localStorage.getItem("data"));
 if (data != null) {
-    var users = data.users;
-    for (var i in users) {
-        userCourses = data.users[i].courses;
-        if (users[i].id == data.current_user && userCourses.length !== 0) {
-            for (var courseId in userCourses) {
-                var currentCourse = courses.courses.find(
-                    (course) => course.id === userCourses[courseId]
-                );
+  var users = data.users;
+  for (var i in users) {
+    userCourses = data.users[i].courses;
+    if (users[i].id == data.current_user && userCourses.length !== 0) {
+      for (var courseId in userCourses) {
+        var currentCourse = courses.courses.find(
+          (course) => course.id === userCourses[courseId]
+        );
 
-                courseCard = document.createElement("div");
-                courseCard.innerHTML = `<img src="${currentCourse.image}"/>
+        courseCard = document.createElement("div");
+        courseCard.innerHTML = `<img src="${currentCourse.image}"/>
                                     <h2 class="course-name">${currentCourse.name}</h2>
                                     <p class="instructor">${currentCourse.instructor}</p>
                                     <p class="rating">Rating: ${currentCourse.rating}</p>`;
-                container.appendChild(courseCard);
-            }
-        } else if (
-            users[i].id == data.current_user &&
-            userCourses.length == 0
-        ) {
-            pragraph.innerText = "no courses added";
-            pragraph.className = "no-courses";
-            container.appendChild(pragraph);
-        }
+        container.appendChild(courseCard);
+      }
+    } else if (users[i].id == data.current_user && userCourses.length == 0) {
+      pragraph.innerText = "no courses added";
+      pragraph.className = "no-courses";
+      container.appendChild(pragraph);
     }
+  }
 } else {
-    pragraph.innerHTML = `<p>no courses added you aren't log in please <a href="../login/login.html">log in</a>`;
-    pragraph.className = "no-courses";
-    container.appendChild(pragraph);
+  pragraph.innerHTML = `<p>no courses added you aren't log in please <a href="../login/login.html">log in</a>`;
+  pragraph.className = "no-courses";
+  container.appendChild(pragraph);
 }
-
 
 // addition for nav
 
@@ -53,20 +49,26 @@ user_icon.onclick = function disp() {
   dropDown.style.display = dropDown.style.display != "flex" ? "flex" : "none";
 };
 
+// log in and out
 
-// log in and out 
-var data = JSON.parse(localStorage.getItem("data"))
-var isloggedIn = data.current_user || 0 
-var register = document.getElementById('Profile_Page')
-var log = document.getElementById('log')
-if (isloggedIn === 0 ){
-    register.innerHTML = `<i  class="fa-solid fa-circle-user"></i> Register`
-    register.setAttribute('href','../singup/index.html')
-    log.innerHTML = `<i class="fa-solid fa-right-to-bracket"></i> Login`
-    log.setAttribute('href','../login/login.html')
+var localstorageData = JSON.parse(localStorage.getItem("data")) || {
+  current_user: 0,
+  users: [],
+};
+localStorage.setItem("data", JSON.stringify(localstorageData));
+
+var data = JSON.parse(localStorage.getItem("data"));
+var isloggedIn = data.current_user || 0;
+var register = document.getElementById("Profile_Page");
+var log = document.getElementById("log");
+if (isloggedIn === 0) {
+  register.innerHTML = `<i  class="fa-solid fa-circle-user"></i> Register`;
+  register.setAttribute("href", "../singup/index.html");
+  log.innerHTML = `<i class="fa-solid fa-right-to-bracket"></i> Login`;
+  log.setAttribute("href", "../login/login.html");
 } else {
-  register.innerHTML = `<i  class="fa-solid fa-circle-user"></i> Profile`
-  register.setAttribute('href','../profile page/index.html')
-  log.innerHTML = `<i class="fa-solid fa-right-to-bracket"></i> Logout`
-  log.setAttribute('href','../login/login.html')
+  register.innerHTML = `<i  class="fa-solid fa-circle-user"></i> Profile`;
+  register.setAttribute("href", "../profile page/index.html");
+  log.innerHTML = `<i class="fa-solid fa-right-to-bracket"></i> Logout`;
+  log.setAttribute("href", "../login/login.html");
 }
