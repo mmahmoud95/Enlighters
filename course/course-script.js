3e72809a59620f08cd98f7fd447b6de715e88971
mustafamhmod95@gmail.com <mustafamhmod95@gmail.com>
2023-08-13 17:41:02 +0300

    Edit to prevent add course if user doesn't login

--- course/course-script.js
+++ course/course-script.js
@@ -4,13 +4,13 @@ var courseID = window.location.href.split("?")[1].split("=")[1];
 var courseContainer = document.getElementById("course");
 
 var currentCourse = courses.courses.find((course) => course.id == courseID);
-var content = document.createTextNode("Enroll");
+var content = document.createTextNode("");
 courseContainer.innerHTML = `
         <div class="name">
             <h1>${currentCourse.name}</h1>
-             <form action="../myCourses/index.html">
-               <button id="enroll" class="enroll-btn" type="submit">${content.nodeValue}</button>
-             </form>
+        
+               <button id="enroll" class="enroll-btn" typy="submit">  <a href="../myCourses/index.html">Enroll</a></button>
+       
         </div>
         <p> Created By: ${currentCourse.instructor}</p>
         <div class='des'>
@@ -21,27 +21,38 @@ courseContainer.innerHTML = `
             </div>
         </div>
 `;
+
+//////////////////////////////////////////////////////////////push course to my courses page
 var data = JSON.parse(localStorage.getItem("data"));
 var currentuserID = data.current_user;
+
 var userIndex = data.users.findIndex((user) => user.id === currentuserID);
-var coursesUser = data.users[userIndex].courses;
-console.log(coursesUser);
+console.log(userIndex);
+if (currentuserID != 0) {
+    var myCourses = data.users[userIndex].courses;
+}
+///////////////////////
 var enroll = document.getElementById("enroll");
-for (const i in coursesUser) {
-    if (coursesUser[i] == currentCourse.id) {
+if (data != null && currentuserID != 0) {
+    enroll.addEventListener("click", () => {
+        console.log(data.users);
+        myCourses = data.users[userIndex].courses || [];
+        myCourses.push(currentCourse.id);
+        data.users[userIndex].courses = myCourses;
+        localStorage.setItem("data", JSON.stringify(data)); //override
+        alert("your have enrolled now - page redirect to My Courses page");
+    });
+} else {
+    enroll.addEventListener("mouseover", () => {
+        alert("you have to login");
+    });
+}
+////////////////////////
+for (const i in myCourses) {
+    if (myCourses[i] == currentCourse.id) {
         enroll.classList.add("enrolled");
         enroll.addEventListener("mouseenter", () => {
             alert("You can not Enroll Again - Check My Courses page");
         });
-        content.nodeValue = "s";
-        console.log(content.nodeValue);
     }
 }
-
-enroll.addEventListener("click", () => {
-    var myCourses = data.users[userIndex].courses || [];
-    myCourses.push(currentCourse.id);
-    data.users[userIndex].courses = myCourses;
-    localStorage.setItem("data", JSON.stringify(data)); //override
-    alert("your have enrolled now - page redirect to My Courses page");
-});
--- myCourses/script.js
+++ myCourses/script.js
@@ -6,33 +6,36 @@ const pragraph = document.createElement("p");
 var courseCard;
 var userCourses;
 var data = JSON.parse(localStorage.getItem("data"));
-if (data != null) {
-  var users = data.users;
-  for (var i in users) {
-    userCourses = data.users[i].courses;
-    if (users[i].id == data.current_user && userCourses.length !== 0) {
-      for (var courseId in userCourses) {
-        var currentCourse = courses.courses.find(
-          (course) => course.id === userCourses[courseId]
-        );
-
-        courseCard = document.createElement("div");
-        courseCard.innerHTML = `<img src="${currentCourse.image}"/>
+console.log(data.current_user);
+if (data != null && data.current_user != 0) {
+    var users = data.users;
+    for (var i in users) {
+        userCourses = data.users[i].courses;
+        if (users[i].id == data.current_user && userCourses.length !== 0) {
+            for (var courseId in userCourses) {
+                var currentCourse = courses.courses.find(
+                    (course) => course.id === userCourses[courseId]
+                );
+                courseCard = document.createElement("div");
+                courseCard.innerHTML = `<img src="${currentCourse.image}"/>
                                     <h2 class="course-name">${currentCourse.name}</h2>
                                     <p class="instructor">${currentCourse.instructor}</p>
                                     <p class="rating">Rating: ${currentCourse.rating}</p>`;
-        container.appendChild(courseCard);
-      }
-    } else if (users[i].id == data.current_user && userCourses.length == 0) {
-      pragraph.innerText = "no courses added";
-      pragraph.className = "no-courses";
-      container.appendChild(pragraph);
+                container.appendChild(courseCard);
+            }
+        } else if (
+            users[i].id == data.current_user &&
+            userCourses.length == 0
+        ) {
+            pragraph.innerText = "no courses added";
+            pragraph.className = "no-courses";
+            container.appendChild(pragraph);
+        }
     }
-  }
 } else {
-  pragraph.innerHTML = `<p>no courses added you aren't log in please <a href="../login/login.html">log in</a>`;
-  pragraph.className = "no-courses";
-  container.appendChild(pragraph);
+    pragraph.innerHTML = `<p>no courses added you aren't log in please <a href="../login/login.html">log in</a>`;
+    pragraph.className = "no-courses";
+    container.appendChild(pragraph);
 }
 
 // addition for nav
@@ -40,20 +43,20 @@ if (data != null) {
 var logo = document.getElementById("logo");
 
 logo.onclick = function () {
-  window.location.href = "./";
+    window.location.href = "./";
 };
 
 var dropDown = document.getElementById("user");
 var user_icon = document.getElementById("user_icon");
 user_icon.onclick = function disp() {
-  dropDown.style.display = dropDown.style.display != "flex" ? "flex" : "none";
+    dropDown.style.display = dropDown.style.display != "flex" ? "flex" : "none";
 };
 
 // log in and out
 
 var localstorageData = JSON.parse(localStorage.getItem("data")) || {
-  current_user: 0,
-  users: [],
+    current_user: 0,
+    users: [],
 };
 localStorage.setItem("data", JSON.stringify(localstorageData));
 
@@ -62,13 +65,13 @@ var isloggedIn = data.current_user || 0;
 var register = document.getElementById("Profile_Page");
 var log = document.getElementById("log");
 if (isloggedIn === 0) {
-  register.innerHTML = `<i  class="fa-solid fa-circle-user"></i> Register`;
-  register.setAttribute("href", "../singup/index.html");
-  log.innerHTML = `<i class="fa-solid fa-right-to-bracket"></i> Login`;
-  log.setAttribute("href", "../login/login.html");
+    register.innerHTML = `<i  class="fa-solid fa-circle-user"></i> Register`;
+    register.setAttribute("href", "../singup/index.html");
+    log.innerHTML = `<i class="fa-solid fa-right-to-bracket"></i> Login`;
+    log.setAttribute("href", "../login/login.html");
 } else {
-  register.innerHTML = `<i  class="fa-solid fa-circle-user"></i> Profile`;
-  register.setAttribute("href", "../profile page/index.html");
-  log.innerHTML = `<i class="fa-solid fa-right-to-bracket"></i> Logout`;
-  log.setAttribute("href", "../login/login.html");
+    register.innerHTML = `<i  class="fa-solid fa-circle-user"></i> Profile`;
+    register.setAttribute("href", "../profile page/index.html");
+    log.innerHTML = `<i class="fa-solid fa-right-to-bracket"></i> Logout`;
+    log.setAttribute("href", "../login/login.html");
 }
